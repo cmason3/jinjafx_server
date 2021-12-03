7a20cc096868e3ecd4f453ec9ee96e0c1130129d
Chris Mason <chris@netnix.org>
2021-12-03 00:03:43 +0000

    add to pypi

---
+++ setup.py
@@ -0,0 +1,31 @@
+import pathlib, re
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+README = re.sub(r'<p.+</p>', '', README[README.find('#'):])
+
+setup(
+  name="jinjafx_server",
+  version="21.11.0",
+  description="Jinja2 Templating Tool",
+  long_description=README,
+  long_description_content_type="text/markdown",
+  url="https://github.com/cmason3/jinjafx_server",
+  author="Chris Mason",
+  author_email="chris@netnix.org",
+  license="MIT",
+  classifiers=[
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+  ],
+  packages=["jinjafx_server"],
+  include_package_data=True,
+  install_requires=["jinjafx", "ansible-core", "netaddr", "requests"],
+  entry_points={
+    "console_scripts": [
+      "jinjafx_server=jinjafx_server:main",
+    ]
+  }
+)
