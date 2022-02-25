39720a3b76285279055b30110e231603779f67a3
Chris Mason <chris.mason@telefonica.com>
2022-02-25 16:25:55 +0000

    add initial support for a lambda version of jinjafx

---
+++ lambda_function.py
@@ -0,0 +1,75 @@
+# JinjaFx Lambda - Jinja2 Templating Tool
+# Copyright (c) 2020-2022 Chris Mason <chris@netnix.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+# zip jinjafx_lambda.zip lambda_function.py www/*
+
+import os, re
+
+def lambda_handler(event, context):
+  method = event['requestContext']['http']['method']
+  pathname = event['rawPath']
+  
+  if method == 'GET':
+    if pathname == '/':
+      pathname = '/index.html'
+  
+    if re.search(r'^/[A-Z0-9_-]+\.[A-Z0-9]+$', pathname, re.IGNORECASE) and os.path.isfile(os.environ['LAMBDA_TASK_ROOT'] + '/www' + pathname):
+      with open(os.environ['LAMBDA_TASK_ROOT'] + '/www' + pathname, 'rb') as f:
+        isBase64Encoded = False
+        headers = {}
+        
+        data = f.read()
+
+        if pathname == '/index.html':
+          data = data.decode('utf-8').replace('{{ jinjafx.version }}', 'JinjaFx vX.Y.Z / Jinja2 vX.Y.Z').replace('{{ get_link }}', 'false').encode('utf-8')
+
+        if pathname.endswith('.js'):
+          headers['content-type'] = 'text/javascript'
+        elif pathname.endswith('.css'):
+          headers['content-type'] = 'text/css'
+        elif pathname.endswith('.png'):
+          import base64
+          headers['content-type'] = 'image/png'
+          data = base64.b64encode(data).decode('utf-8')
+          isBase64Encoded = True
+        else:
+          headers['content-type'] = 'text/html'
+    
+        return {
+          "headers": headers,
+          "statusCode": 200,
+          "isBase64Encoded": isBase64Encoded,
+          "body": data
+        }
+    
+    return {
+      "statusCode": 404,
+      "body": "Not Found"
+    }
+
+  elif method == 'POST':
+    if pathname == '/jinjafx':
+      pass
+ 
+    return {
+      "statusCode": 404,
+      "body": "Not Found"
+    }
+   
+  return {
+    "statusCode": 501,
+    "body": "Not Implemented"
+  }
+
