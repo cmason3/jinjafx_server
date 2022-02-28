c257f1719cfae4520acb977333e4eb3fc36f7779
Chris Mason <chris.mason@virginmediao2.co.uk>
2022-02-28 23:17:01 +0000

    update lambda

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -78,7 +78,7 @@ def lambda_handler(event, context):
     
     return {
       "statusCode": 404,
-      "body": "Not Found - " + str(event)
+      "body": "Not Found"
     }
 
   elif method == 'POST':
