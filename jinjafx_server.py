a08534c0fb597a03143ca0d906db3e40a1d13389
Chris Mason <57402469+cmason3@users.noreply.github.com>
2022-12-19 09:52:18 +0000

    update version

--- jinjafx_server.py
+++ jinjafx_server.py
@@ -21,7 +21,7 @@ import jinjafx, os, io, sys, socket, signal, threading, yaml, json, base64, time
 import re, argparse, zipfile, hashlib, traceback, glob, hmac, uuid, struct, binascii, gzip, requests
 import cmarkgfm, emoji, func_timeout
 
-__version__ = '23.1.0-dev'
+__version__ = '23.1.0'
 
 lock = threading.RLock()
 base = os.path.abspath(os.path.dirname(__file__))
