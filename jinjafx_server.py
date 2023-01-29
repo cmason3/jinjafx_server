961de84922bbdb0ba996932f2cdb8f9a2a363f52
Chris Mason <chris@netnix.org>
2023-01-29 09:59:42 +0000

    version bump

--- jinjafx_server.py
+++ jinjafx_server.py
@@ -21,7 +21,7 @@ import jinjafx, os, io, sys, socket, signal, threading, yaml, json, base64, time
 import re, argparse, zipfile, hashlib, traceback, glob, hmac, uuid, struct, binascii, gzip, requests, ctypes
 import cmarkgfm, emoji
 
-__version__ = '23.1.0'
+__version__ = '23.2.0'
 
 lock = threading.RLock()
 base = os.path.abspath(os.path.dirname(__file__))
