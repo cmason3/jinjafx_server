18589037b635334dc4068c731f20e06951078b7a
Cloud User <ec2-user@jinjafx.io>
2023-08-08 16:05:35 +0100

    add x-forwarded-proto

--- jinjafx_server.py
+++ jinjafx_server.py
@@ -21,7 +21,7 @@ import jinjafx, os, io, sys, socket, signal, threading, yaml, json, base64, time
 import re, argparse, hashlib, traceback, glob, hmac, uuid, struct, binascii, gzip, requests, ctypes
 import cmarkgfm, emoji
 
-__version__ = '23.8.1'
+__version__ = '23.8.2'
 
 lock = threading.RLock()
 base = os.path.abspath(os.path.dirname(__file__))
@@ -82,11 +82,15 @@ class JinjaFxRequest(BaseHTTPRequestHandler):
 
         if (args[1] != '204' and args[1] != '404' and args[1] != '501') or self.critical or verbose:
           src = str(self.client_address[0])
+          proto = ''
           ctype = ''
 
           if hasattr(self, 'headers'):
             if 'X-Forwarded-For' in self.headers:
               src = self.headers['X-Forwarded-For']
+
+            if 'X-Forwarded-Proto' in self.headers:
+              proto = ':' + self.headers['X-Forwarded-Proto'] + ' '
 
             if 'Content-Type' in self.headers:
               if 'Content-Encoding' in self.headers:
@@ -101,13 +105,13 @@ class JinjaFxRequest(BaseHTTPRequestHandler):
               ae = ''
 
             if self.elapsed is not None:
-              log('[' + src + '] [\033[1;' + ansi + 'm' + str(args[1]) + '\033[0m]' + ' \033[1;33m' + self.command + '\033[0m ' + path + ctype + ' [' + self.format_bytes(self.length) + '] in ' + str(self.elapsed) + 'ms', ae)
+              log('[' + src + ']' + proto + ' [\033[1;' + ansi + 'm' + str(args[1]) + '\033[0m]' + ' \033[1;33m' + self.command + '\033[0m ' + path + ctype + ' [' + self.format_bytes(self.length) + '] in ' + str(self.elapsed) + 'ms', ae)
             else:
-              log('[' + src + '] [\033[1;' + ansi + 'm' + str(args[1]) + '\033[0m]' + ' \033[1;33m' + self.command + '\033[0m ' + path + ctype + ' [' + self.format_bytes(self.length) + ']', ae)
+              log('[' + src + ']' + proto + ' [\033[1;' + ansi + 'm' + str(args[1]) + '\033[0m]' + ' \033[1;33m' + self.command + '\033[0m ' + path + ctype + ' [' + self.format_bytes(self.length) + ']', ae)
 
           elif self.command != None:
             if (args[1] != '200' and args[1] != '304') or (not path.endswith('.js') and not path.endswith('.css') and not path.endswith('.png')) or verbose:
-              log('[' + src + '] [\033[1;' + ansi + 'm' + str(args[1]) + '\033[0m]' + ' ' + self.command + ' ' + path)
+              log('[' + src + ']' + proto + ' [\033[1;' + ansi + 'm' + str(args[1]) + '\033[0m]' + ' ' + self.command + ' ' + path)
 
 
   def encode_link(self, bhash):
