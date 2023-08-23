993eb381731095b223a49db096fc0fb15caa1816
Cloud User <ec2-user@jinjafx.io>
2023-08-23 08:24:24 +0100

    Add support for keyless YAML via the `_` variable

--- jinjafx_server.py
+++ jinjafx_server.py
@@ -23,7 +23,7 @@ import jinjafx, os, io, sys, socket, signal, threading, yaml, json, base64, time
 import re, argparse, hashlib, traceback, glob, hmac, uuid, struct, binascii, gzip, requests, ctypes
 import cmarkgfm, emoji
 
-__version__ = '23.8.2'
+__version__ = '23.8.3'
 
 llock = threading.RLock()
 rlock = threading.RLock()
@@ -465,6 +465,9 @@ class JinjaFxRequest(BaseHTTPRequestHandler):
 
                 y = yaml.load(gyaml, Loader=yaml.SafeLoader)
                 if y != None:
+                  if isinstance(y, list):
+                    y = {'_': y}
+
                   gvars.update(y)
 
               st = round(time.time() * 1000)
