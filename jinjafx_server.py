05af8c1a4e188848e24d87dc301d0510fb8eaa49
Chris Mason <chris.mason@virginmediao2.co.uk>
2023-09-21 08:56:35 +0100

    fix further deprecation of utcnow

--- jinjafx_server.py
+++ jinjafx_server.py
@@ -1083,7 +1083,8 @@ def aws_s3_get(s3_url, fname):
     'Host': s3_url,
     'Accept-Encoding': 'gzip',
     'x-amz-content-sha256': hashlib.sha256(b'').hexdigest(),
-    'x-amz-date': datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
+    'x-amz-date': datetime.datetime.now(datetime.timezone.utc).strftime('%Y%m%dT%H%M%SZ')
+#    'x-amz-date': datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
   }
   headers = aws_s3_authorization('GET', fname, s3_url.split('.')[2], headers)
   return requests.get('https://' + s3_url + '/' + fname, headers=headers)
