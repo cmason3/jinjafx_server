63789f2d51a02289bbb26b60dd5c16596a916437
Chris Mason <chris@netnix.org>
2024-07-20 18:30:47 +0100

    bump version

--- jinjafx_server.py
+++ jinjafx_server.py
@@ -28,7 +28,7 @@ import jinjafx, os, io, socket, signal, threading, yaml, json, base64, time, dat
 import re, argparse, hashlib, traceback, glob, hmac, uuid, struct, binascii, gzip, requests, ctypes, subprocess
 import cmarkgfm, emoji
 
-__version__ = '24.6.4'
+__version__ = '24.7.0'
 
 llock = threading.RLock()
 rlock = threading.RLock()
