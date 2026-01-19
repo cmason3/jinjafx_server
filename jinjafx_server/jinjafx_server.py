832df39eca097781347d1b7488bbaf2cf03139bd
Chris Mason <chris@netnix.org>
2026-01-19 07:29:28 +0000

    fix readonly panes

--- jinjafx_server/jinjafx_server.py
+++ jinjafx_server/jinjafx_server.py
@@ -28,7 +28,7 @@ import jinjafx, os, io, socket, signal, threading, yaml, json, base64, time, dat
 import re, argparse, hashlib, traceback, glob, hmac, uuid, struct, binascii, gzip, requests, ctypes, subprocess
 import cmarkgfm, emoji, jsonschema
 
-__version__ = '26.2.2'
+__version__ = '26.2.3'
 
 llock = threading.RLock()
 rlock = threading.RLock()
